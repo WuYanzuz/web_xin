9e17f2cb80a4c83b92709082f806a69e4dba33c4
WuYanzu <1931295119@qq.com>
2022-05-22 17:38:25 +0800

    模型搭建完成

---
+++ phos/src/main/java/edu/jxut/phos/config/SpringConfig.java
@@ -0,0 +1,25 @@
+package edu.jxut.phos.config;
+
+import edu.jxut.phos.mapper.NewsMapper;
+import edu.jxut.phos.pojos.News;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Author MengYuan Zhang
+ * @Date 2022/5/21 15:30
+ * @CreatedBy IntelliJ IDEA
+ * @Details
+ */
+//@Component
+public class SpringConfig implements ApplicationContextAware {
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
+
+}
--- phos/src/main/java/edu/jxut/phos/config/WebConfig.java
+++ phos/src/main/java/edu/jxut/phos/config/WebConfig.java
@@ -4,6 +4,7 @@ package edu.jxut.phos.config;
 import edu.jxut.phos.interceptor.WebInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -29,5 +30,21 @@ public class WebConfig extends WebMvcConfigurationSupport {
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new WebInterceptor())
                 .addPathPatterns("/**");
+    }
+
+    //跨域支持
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                //放行哪些原始域
+                .allowedOriginPatterns("*")
+                //是否发送Cookie信息
+                .allowCredentials(true)
+                //放行哪些原始域(请求方式)
+                .allowedMethods("GET","POST", "PUT", "DELETE")
+                //放行哪些原始域(头部信息)
+                .allowedHeaders("*")
+                //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                .exposedHeaders("Header1", "Header2");
     }
 }
